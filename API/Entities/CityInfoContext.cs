10fa88de4d682f1810a667638a5cdcc655716cc9
Dillon, Joseph <joseph.dillon.522@gmail.com>
2018-11-12 15:12:19 -0500

    added seeds

--- API/Entities/CityInfoContext.cs
+++ API/Entities/CityInfoContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using API.Entities;
 
 namespace API.Entities
 {
@@ -17,6 +18,47 @@ namespace API.Entities
 
         //     base.OnConfiguring(optionsBuilder);
         // }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            #region CitySeed
+            modelBuilder.Entity<City>().HasData(new City {
+                Id = 1,
+                Name = "Seeded City Name",
+                Description = "Lets see if this works"
+            });
+            #endregion
+
+            modelBuilder.Entity<Point>(entity =>
+            {
+                entity.Property(d => d.Name).IsRequired();
+            });
+
+            #region PointSeed
+            modelBuilder.Entity<Point>().HasData(
+                new Point() {
+                    Id = 1,
+                    CityId = 1,
+                    Name = "First Point",
+                    Description = "Test 1"
+                });
+            #endregion
+
+            #region AnonymousPointSeed
+            modelBuilder.Entity<Point>().HasData(
+                new {
+                    Id = 2,
+                    CityId = 1,
+                    Name = "Second Point",
+                    Description = "Test 2"
+                });
+            #endregion
+        }
 
     }
 }
---
+++ API/Migrations/20181112200829_TestSeedDB.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class TestSeedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Lets see if this works", "Seeded City Name" });
+
+            migrationBuilder.InsertData(
+                table: "Points",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 1, 1, "Test 1", "First Point" });
+
+            migrationBuilder.InsertData(
+                table: "Points",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 2, 1, "Test 2", "Second Point" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Points",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Points",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
